d1bec6c88ea33afb7c4eb23fdddc749449762a62
kristiantaraj <tarajkristian@gmail.com>
2021-01-29 10:11:29 +0100

    Small Fixes

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,18 @@
-var submit = document.getElementById('_form_3_submit');
 
-submit.innerHTML = '<span style="font-size: 20px; margin-right: 5px;">Invia</span> <img style="margin-top:-2px;" src="assets/plane.png">';
+       $( document ).ready(function() {
+
+        $('#_form_3_submit').attr('disabled', 'disabled');
+           
+
+        $('#check').click(function() {
+        if ($(this).is(':checked')) {
+        $('#_form_3_submit').removeAttr('disabled');
+        } else {
+        $('#_form_3_submit').attr('disabled', 'disabled');
+            }
+        });
+    });
+
+    var submit = document.getElementById('_form_3_submit');
+
+    submit.innerHTML = '<span style="font-size: 20px; margin-right: 5px;">Invia</span> <img style="margin-top:-2px;" src="assets/plane.png">';
